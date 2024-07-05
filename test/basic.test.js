b972c8ce651dbf8b7baa71d9c88f47cc9aad9ec4
Norman Strassner <nstrassner@asrcfederal.com>
2024-07-05 12:48:09 -0600

    Forgot to add test files

---
+++ test/basic.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "vitest";
+//import { setup } from "@nuxt/test-utils-edge";
+
+describe("My test", async () => {
+  test("my test", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
