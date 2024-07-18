4fcc36f9ac8e3600ec36e6044d7a2e8be70aea15
Norman Strassner <nstrassner@asrcfederal.com>
2024-07-18 12:48:44 -0600

    Added dropdowns to the Headwater Basin Gage tab.

--- components/HeadwaterBasinGage.vue
+++ components/HeadwaterBasinGage.vue
@@ -1,21 +1,75 @@
 <template>
-    <div class="waitgif" v-if="loading">
-      <img src="/assets/styles/img/wait.gif" />
+  <div id="GageGroup" class="full-width mt-5 ml-20">
+    <div class="grid grid-rows-12">
+      <div class="row-span-4">
+        <div class="grid grid-cols-4 gap=1">
+          <div class="col-span-1">
+            <label for="domain_dd">Domain: </label><br />
+            <select id="domain_dd">
+              <option value="" disabled selected>...</option>
+              <option>Select Domain</option>
+            </select>
+          </div>
+          <div class="col-span-1">
+            <label for="gage_dd">Gage: </label><br />
+            <select id="gage_dd">
+              <option value="" disabled selected>...</option>
+              <option>Select Gage</option>
+            </select>
+          </div>
+          <div class="col-span-1">
+            <label for="forcing_dd">Forcing: </label><br />
+            <select id="forcing_dd">
+              <option value="" disabled selected>...</option>
+              <option value="upload">Upload</option>
+              <option value="aorc">AORC</option>
+            </select>
+          </div>
+          <div class="col-span-1">
+            <label for="observational_dd">Observational: </label><br />
+            <select id="observational_dd">
+              <option value="" disabled selected>...</option>
+              <option value="upload">Upload</option>
+              <option value="usgs">USGS</option>
+              <option value="usace">USACE</option>
+              <option value="ca_dwr">CA DWR</option>
+              <option value="env_canada">Environment Canada</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="row-span-8"></div>
     </div>
+  </div>
+  <div class="waitgif" v-if="loading">
+    <img src="@/assets/styles/img/wait.gif" />
+  </div>
 </template>
 <script lang="ts" setup>
-import  Tabs from "@/components/Tabs.vue"
+import Tabs from "@/components/Tabs.vue";
 
 const loading = ref(true);
 const showMap = ref(false);
 
 onMounted(() => {
   setTimeout(() => {
-  loading.value = false;
-}, 500);
+    loading.value = false;
+  }, 500);
 });
 </script>
 <style lang="scss" scoped>
+@import "@/assets/styles/styles.scss";
+#GageGroup {
+  background-color: #cccccc;
+  padding: 10px 10px 10px 20px;
+  border-radius: 20px;
+  height: 80px;
+  border: 1px solid $ngwcp_primary1;
+}
+select {
+  width: 90%;
+}
 .waitgif {
   position: absolute;
   top: 40%;
--- components/ProgressGroup.vue
+++ components/ProgressGroup.vue
@@ -3,35 +3,35 @@
     <table id="ProgressTable">
       <tr>
         <td class="ptype">Headwater Basin Gage</td>
-        <td><input type="checkbox" v-model="props.progress.headwater_basin_gage" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.headwater_basin_gage" /></td>
       </tr>
       <tr>
         <td class="ptype">Formulation</td>
-        <td><input type="checkbox" v-model="props.progress.formulation" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.formulation" /></td>
       </tr>
       <tr>
         <td class="ptype">Start and End Times</td>
-        <td><input type="checkbox" v-model="props.progress.start_and_end_times" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.start_and_end_times" /></td>
       </tr>
       <tr>
         <td class="ptype">calibration Output Variable</td>
-        <td><input type="checkbox" v-model="props.progress.calibration_stop_criteria" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.calibration_stop_criteria" /></td>
       </tr>
       <tr>
         <td class="ptype">Tuning Paramters</td>
-        <td><input type="checkbox" v-model="props.progress.tuning_parameters" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.tuning_parameters" /></td>
       </tr>
       <tr>
         <td class="ptype">optimization Algorithm</td>
-        <td><input type="checkbox" v-model="props.progress.optimization_algorithm" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.optimization_algorithm" /></td>
       </tr>
       <tr>
         <td class="ptype">Objective Function</td>
-        <td><input type="checkbox" v-model="props.progress.objective_function" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.objective_function" /></td>
       </tr>
       <tr>
         <td class="ptype">calibration Stop Criteria</td>
-        <td><input type="checkbox" v-model="props.progress.calibration_stop_criteria" /></td>
+        <td><input tabindex="-1" readonly type="checkbox" v-model="props.progress.calibration_stop_criteria" /></td>
       </tr>
       <tr>
         <td class="ptype">Metrics / Plot Inteval</td>
