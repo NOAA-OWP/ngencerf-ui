2142b99324eb99782884ef864bb52c7fbd964018
Norman Strassner <nstrassner@asrcfederal.com>
2024-07-22 08:03:15 -0600

    Added info box to Headwater Basin Gage

--- components/HeadwaterBasinGage.vue
+++ components/HeadwaterBasinGage.vue
@@ -1,45 +1,87 @@
 <template>
-  <div id="GageGroup" class="mt-5 ml-20">
-    <div class="grid grid-rows-12">
-      <div class="row-span-4">
-        <div class="grid grid-cols-4 gap=1">
-          <div class="col-span-1">
-            <label for="domain_dd">Domain: </label><br />
-            <select id="domain_dd">
-              <option value="" disabled selected>...</option>
-              <option>Select Domain</option>
-            </select>
-          </div>
-          <div class="col-span-1">
-            <label for="gage_dd">Gage: </label><br />
-            <select id="gage_dd">
-              <option value="" disabled selected>...</option>
-              <option>Select Gage</option>
-            </select>
-          </div>
-          <div class="col-span-1">
-            <label for="forcing_dd">Forcing: </label><br />
-            <select id="forcing_dd">
-              <option value="" disabled selected>...</option>
-              <option value="upload">Upload</option>
-              <option value="aorc">AORC</option>
-            </select>
-          </div>
-          <div class="col-span-1">
-            <label for="observational_dd">Observational: </label><br />
-            <select id="observational_dd">
-              <option value="" disabled selected>...</option>
-              <option value="upload">Upload</option>
-              <option value="usgs">USGS</option>
-              <option value="usace">USACE</option>
-              <option value="ca_dwr">CA DWR</option>
-              <option value="env_canada">Environment Canada</option>
-            </select>
+  <div id="HeadwaterBasinGage">
+    <div id="GageSettings" class="mt-5 ml-20">
+      <div class="grid grid-rows-12">
+        <div class="row-span-4">
+          <div class="grid grid-cols-4 gap=1">
+            <div class="col-span-1">
+              <label for="domain_dd">Domain: </label><br />
+              <select id="domain_dd">
+                <option value="" disabled selected>...</option>
+                <option>Select Domain</option>
+              </select>
+            </div>
+            <div class="col-span-1">
+              <label for="gage_dd">Gage: </label><br />
+              <select id="gage_dd">
+                <option value="" disabled selected>...</option>
+                <option>Select Gage</option>
+              </select>
+            </div>
+            <div class="col-span-1">
+              <label for="forcing_dd">Forcing: </label><br />
+              <select id="forcing_dd">
+                <option value="" disabled selected>...</option>
+                <option value="upload">Upload</option>
+                <option value="aorc">AORC</option>
+              </select>
+            </div>
+            <div class="col-span-1">
+              <label for="observational_dd">Observational: </label><br />
+              <select id="observational_dd">
+                <option value="" disabled selected>...</option>
+                <option value="upload">Upload</option>
+                <option value="usgs">USGS</option>
+                <option value="usace">USACE</option>
+                <option value="ca_dwr">CA DWR</option>
+                <option value="env_canada">Environment Canada</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
-
-      <div class="row-span-8"></div>
+    </div>
+    <div id="GageReport">
+      <div id="GrBox">
+        <table>
+          <tr class="rowOdd">
+            <td class="dataName">Agency</td>
+            <td class="dataText">{{ gageData.agency }}</td>
+          </tr>
+          <tr class="rowEven">
+            <td class="dataName">Station Name</td>
+            <td class="dataText">{{ gageData.station_name }}</td>
+          </tr>
+          <tr class="rowOdd">
+            <td class="dataName">Site Type</td>
+            <td class="dataText">{{ gageData.site_type }}</td>
+          </tr>
+          <tr class="rowEven">
+            <td class="dataName">Latitude</td>
+            <td class="dataText">{{ gageData.latitude }}</td>
+          </tr>
+          <tr class="rowOdd">
+            <td class="dataName">Longitude</td>
+            <td class="dataText">{{ gageData.longitude }}</td>
+          </tr>
+          <tr class="rowEven">
+            <td class="dataName">Altitude</td>
+            <td class="dataText">{{ gageData.altitude }}</td>
+          </tr>
+          <tr class="rowOdd">
+            <td class="dataName">Date Established</td>
+            <td class="dataText">{{ gageData.date_established }}</td>
+          </tr>
+          <tr class="rowEven">
+            <td class="dataName">Drainage Area</td>
+            <td class="dataText">{{ gageData.drainage_area }}</td>
+          </tr>
+          <tr class="rowOdd">
+            <td class="dataName">HUC</td>
+            <td class="dataText">{{ gageData.huc }}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
   <div class="waitgif" v-if="loading">
@@ -52,6 +94,18 @@ import Tabs from "@/components/Tabs.vue";
 const loading = ref(true);
 const showMap = ref(false);
 
+const gageData = {
+  agency: "US Geological Survey",
+  station_name: "LITTLE RIVER NEAR HANNOVER, CT",
+  site_type: "Stream",
+  latitude: "41.6717651",
+  longitude: "-72.05229807",
+  altitude: "220.32 feet",
+  date_established: "19510701",
+  drainage_area: "30.0 Square miles",
+  huc: "01100002",
+};
+
 onMounted(() => {
   setTimeout(() => {
     loading.value = false;
@@ -60,7 +114,7 @@ onMounted(() => {
 </script>
 <style lang="scss" scoped>
 @import "@/assets/styles/styles.scss";
-#GageGroup {
+#GageSettings {
   background-color: $ngwcp_groupsbkg;
   padding: 10px 10px 10px 20px;
   border-radius: 20px;
@@ -69,6 +123,34 @@ onMounted(() => {
 }
 select {
   width: 90%;
+}
+
+#GageReport {
+  margin: 30px 0 0 118px;
+  width: 600px;
+  height: 315px;
+  background-color: $ngwcp_groupsbkg;
+  border-radius: 20px;
+  border: 1px solid $ngwcp_primary1;
+  padding: 10px;
+  table {
+    width: 100%;
+    margin: 10px auto;
+    border: 1px solid $ngwcp_primary1;
+    tr {
+      line-height: 27px;
+      border-bottom: 1px solid #333333;
+
+      td {
+        width: 50%;
+        padding-left: 5px;
+      }
+    }
+  }
+}
+
+.rowOdd {
+  background-color: #dddddd;
 }
 .waitgif {
   position: absolute;
