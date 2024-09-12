cd151b7b944dadc0583e4dd83096dd3bbbd700c9
Norman Strassner <nstrassner@asrcfederal.com>
2024-09-12 17:15:06 -0600

    Added in help file not added in previous commit

---
+++ components/Help/LandingPageHelp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="_help-page">
+    <div class="_help-title">ngenCERF Landing Page</div>
+    <div class="_help-subtitle">Displays information about the user's previous runs.</div>
+
+    <table class="_help-table">
+      <tr>
+        <td class="td1"></td>
+        <td class="td2"><strong>Displays the number of processes that are currently running</strong></td>
+      </tr>
+      <tr>
+        <td class="td1"></td>
+        <td class="td2"><strong>Displays the number of uncompleted calibration runs</strong></td>
+      </tr>
+      <tr>
+        <td class="td1"></td>
+        <td class="td2">&nbsp;</td>
+      </tr>
+      <tr>
+        <td class="td1"></td>
+        <td class="td2">Click on <strong>Continue</strong> button to see a list of the abovementioned runs
+        </td>
+      </tr>
+
+
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/styles.scss";
+._help-table .td1 {
+  width: 200px;
+}
+</style>
