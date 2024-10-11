2f896dc5cca39560f0937e41c743717b9153c179
Norman Strassner <nstrassner@asrcfederal.com>
2024-10-11 07:16:56 -0600

    Added enums file

---
+++ composables/NextgenEnums.ts
@@ -0,0 +1,33 @@
+
+export const CalibrationTabs =  {
+  tab_calibrationRuns: 0,
+  tab_headwaterBasinGage: 1,
+  tab_formulation: 2,
+  tab_tuningControls: 3,
+  tab_optimizationMetrics: 4,
+  tab_statusRun: 5
+}
+
+export const EvaluationTabs =  {
+  tab_calibrationRuns: 0,
+  tab_evaluate: 1,
+  tab_selectAltIteration: 2,
+}
+
+export const NextgenPages = {
+  page_calibration: 1,
+  page_evaluation: 2,
+  page_forcast: 3,
+  page_verification: 4
+}
+
+export const CalibrationHelpPages = {
+  previousRuns: 1,
+  headwaterBasinGage: 2,
+  formulation: 3,
+  tuningControls: 4,
+  optimizationMetrics: 5,
+  runStatus: 6,
+  results: 7,
+
+}
