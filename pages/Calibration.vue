ddb69be3f299cb4b7431edea716716f39988c82c
Norman Strassner <nstrassner@asrcfederal.com>
2024-11-01 14:38:20 -0600

    Removed refresh warning

--- pages/Calibration.vue
+++ pages/Calibration.vue
@@ -9,19 +9,19 @@
 import { onMounted } from "vue";
 import NextgenLayout from "~/layouts/CalibrationLayout.vue";
 
-onBeforeMount( () => {
-    window.addEventListener("beforeunload", event => {
-      event.preventDefault()
-      return true;
-    })
-  });
+// onBeforeMount( () => {
+//     window.addEventListener("beforeunload", event => {
+//       event.preventDefault()
+//       return true;
+//     })
+//   });
 
-onUnmounted( () => {
-  window.removeEventListener("beforeunload", event => {
-      event.preventDefault()
-      return true;
-    })
-});
+// onUnmounted( () => {
+//   window.removeEventListener("beforeunload", event => {
+//       event.preventDefault()
+//       return true;
+//     })
+// });
 
 onMounted(() => {
   nextTick(() => {
