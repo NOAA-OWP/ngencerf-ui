eb00f5ad0abd1e9138f9d0cacb4440d649eb83eb
Miguel.Pena <miguel.pena@rtx.com>
2024-11-18 00:46:36 -0800

    started work on SetupForecastRunTab

--- components/Forecast/SetupForecastRunTab.vue
+++ components/Forecast/SetupForecastRunTab.vue
@@ -1,67 +1,30 @@
 <template>
-  <div class="h-full min-h-screen ">
-    <div class="grid grid-rows-4">
-      <div class="row-span-2">
-        <div class="grid grid-cols-2  mb-5 mt-10">
-          <div class="col-span-1 ml-10">
-            Select Run Typ:e<br />
-            <div class="mt-5">
-              <RadioButton v-model="observedForcing" inputId="observedForcing" name="observedForcing" />
-              <label for="observedForcing" class="ml-4">Use Observed Forcing</label><br />
-            </div>
-            <div class="mt-5">
-              <RadioButton v-model="forcingSource" inputId="forcingSource" name="forcingSource"></RadioButton>
-              <label for="observedForcing" class="ml-4">Use Forecast Forcing</label>
-            </div>
-          </div>
-
-          <div class="col-span-1">
-            Select Forcing Type:<br />
-            <Dropdown class="mt-5" id="Groups" v-model="forcingType" :options="forcingOptions"></Dropdown>
-          </div>
-        </div>
-      </div>
-
-      <div class="row-span-2 mt-10">
-        <div class="grid grid-cols-2">
-          <div class="col-span-1 ml-10">
-            <div class="mt-5">
-              <label for="observedForcing" class="ml-4">Use Forecast Forcing</label>
-              <Dropdown class="mt-5" id="Groups" v-model="forcingType" :options="forcingOptions"></Dropdown>
-            </div>
-          </div>
-          <div class="col-span-1">
-            <div class="mt-5">
-              <label for="observedForcing" class="ml-4">Use Forecast Forcing</label>
-              <Dropdown class="mt-5" id="Groups" v-model="forcingType" :options="forcingOptions"></Dropdown>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div>
+    <h1 class="mt-10 mb-8 text-3xl font-bold inline-block">
+      Forecast Cycle Selection
+    </h1>
   </div>
-
+  <div>
+    <DataTable sortField="cycle" scrollable v-model:selection="selectedCycle">
+      <Column field="cycle" header="Cycle"></Column>
+      <Column field="data_sources" header="Data Sources"></Column>
+      <Column field="time_range_ngencerf" header="Time Range ngenCERF"></Column>
+    </DataTable>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { hilightTab } from '~/composables/TabHilight';
+
+const selectedCycle = ref();
+
 onMounted(() => {
   hilightTab(ForecastTabs.tab_selectAltIteration);
 
-})
+});
 
-
-const observedForcing = ref();
-const forcingSource = ref();
-
-const forcingType = ref();
-const forcingOptions = ref([]);
 </script>
 
 <style lang="scss" scoped>
-.p-select {
-  width: 300px;
-  border: 1px solid #ccc !important;
-  height: 40px;
-}
+@import "@/assets/styles/styles.scss";
 </style>
