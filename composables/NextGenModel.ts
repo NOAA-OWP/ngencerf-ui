0ffcab3c6cb06128793093c63f8daedbfe123091
David Martin <david.w.martin@U-1I3U6N6N2I2R8.nextgenwaterprediction.com>
2024-11-18 09:51:01 -0800

    Load more plot data when scrolling toward the bottom of the table; assign types to some untyped refs

--- composables/NextGenModel.ts
+++ composables/NextGenModel.ts
@@ -573,6 +573,10 @@ export interface AlternativeIterationTuningParameters {
   [name: string]: string|number;
 }
 
+export interface APIResponse {
+  _data?: {[key: string]: any;};
+}
+
 export interface DynamicObject {
   [key: string]: any;
 }
