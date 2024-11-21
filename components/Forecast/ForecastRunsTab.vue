cbb7a9d0b8c1b619ecfed25db07bf087dbf5cd31
Norman Strassner <nstrassner@asrcfederal.com>
2024-11-21 10:54:23 -0700

    Installed right click menu for Open and Delete for Forecast Previous Calibration Runs

--- components/Forecast/ForecastRunsTab.vue
+++ components/Forecast/ForecastRunsTab.vue
@@ -6,7 +6,7 @@
         <div class="w-2/3">
           <h1 class="pt-3 mb-8 text-3xl font-bold inline-block">
             <span v-if="computedCalibrationValidationRunList.length <= 1">Previous Calibration Runs</span>
-            <span v-if="computedCalibrationValidationRunList.length > 1">Validation Runs for Calibration Job {{
+            <span v-if="computedCalibrationValidationRunList.length > 1">Forecast Runs for Calibration Job {{
               userSelectedEvalCalibrationRunId }}</span>
           </h1>
         </div>
@@ -32,10 +32,14 @@
               </div>
             </div>
 
+            <ConfirmDialog></ConfirmDialog>
+            <ContextMenu :pt="{ root: { id: 'cr-context-menu' } }" class="bg-white" ref="crContextMenu"
+              :model="cmCalibrationRun" @hide="selectedCalibrationRun = undefined"></ContextMenu>
             <DataTable id="cr-list" :value="userEvaluationCalibrationRunListData" scrollable scroll-height="400px"
               sortField="calibration_run_id" :sortOrder="-1" table-style="min-width: 50rem"
               v-model:selection="selectedCalibrationRun" selectionMode="single" :rowStyle="rowStyle"
-              @rowSelect="onEvalCalibrationRowSelect" @rowUnselect="onEvalCalibrationRowUnSelect" class="boxed">
+              @rowSelect="onForecastRowSelect" @rowUnselect="onEvalCalibrationRowUnSelect"
+              @rowContextmenu="onRowContextMenu" class="boxed">
               <Column field="calibration_run_id" header="Run ID" sortable></Column>
               <Column field="status" header="Status" sortable></Column>
               <Column field="submit_date" header="Run Date" sortable>
@@ -75,7 +79,7 @@
 
     </div>
 
-    <div class="waitgif" v-if="isLoadingCalibrationSummary">
+    <div class="waitgif" v-if="isLoading">
       <img alt="Pleae wait..." src="@/assets/styles/img/wait.gif" />
     </div>
   </client-only>
@@ -88,12 +92,27 @@ import type { CalibrationRun, CalibrationValidationJobData } from "~/composables
 import { EvaluationTabs } from "~/composables/NextgenEnums";
 import { useEvaluationCalibrationRunStore } from "~/stores/evaluation/EvaluationCalibrationRunStore";
 import type { DataTableRowClickEvent } from 'primevue/datatable';
-import { storeToRefs } from "pinia";
+import { useCalibrationJobStore } from "~/stores/common/CalibrationJobStore";
 import { useUserDataStore } from "~/stores/common/UserDataStore";
 import { formatDateForDisplay } from '~/utils/TimeHelpers';
 import { hilightTab } from '~/composables/TabHilight';
+import { storeToRefs } from "pinia";
+const { deleteCalibrationRun } = useCalibrationJobStore();
 
 const evaluationCalibrationRunStore = useEvaluationCalibrationRunStore();
+
+const toast = useToast();
+const crContextMenu = ref(); //calibration run context menu
+const contextMenuJob = ref<number>()
+const isLoading = ref(true);
+const cmCalibrationRun = ref([
+  { label: 'Open', icon: 'pi pi-fw-pisearch', command: () => openSelectedCalibrationRun() },
+  { label: 'Delete', icon: 'pi pi-fw-times', command: () => deleteSelectedCalibrationRun() }
+]);
+const onRowContextMenu = (event: any) => {
+  crContextMenu.value.show(event.originalEvent);
+  contextMenuJob.value = parseInt(event.originalEvent.currentTarget.children[0].textContent);
+};
 
 const {
   uiGageId,
@@ -115,32 +134,37 @@ const {
 } = evaluationCalibrationRunStore;
 
 const { userCalibrationRunData } = storeToRefs(useUserDataStore());
-const isLoadingCalibrationSummary = ref<boolean>(false);
 
-const toast = useToast();
 //this model is for highlighting purpose
 const selectedCalibrationRun = ref<CalibrationRun>();
 const selectedCalibrationValidationRun = ref<CalibrationValidationJobData>();
 
 onMounted(() => {
+  isLoading.value = true;
   hilightTab(EvaluationTabs.tab_calibrationRuns);
+  let ele = document.getElementById("MainLeftDataArea") as HTMLElement;
+  if (ele) { ele.scrollTo(0, 0); }
   //clear calibration data if user were on calibraiton tab and clear evaludation previous run data user may have selected
   resetUserSelectedEvalCalibrationRun();
   fetchUserValidatedCalibrationJobsListData();
+  isLoading.value = false;
   console.log("CalibrationRunsTab for Evaluation mounted")
 });
 
-const onEvalCalibrationRowSelect = async (event: DataTableRowClickEvent) => {
+const onForecastRowSelect = async (event: DataTableRowClickEvent) => {
+  isLoading.value = true;
   resetUserSelectedEvalValidationRun();
-  loadSelectedCalibrationRun(event.data.calibration_run_id);
-  isLoadingCalibrationSummary.value = true;
-  fetchUserSelectedCalibrationValidationRunList();
+  nextTick( async () => {
+    await loadSelectedCalibrationRun(event.data.calibration_run_id);
+    await fetchUserSelectedCalibrationValidationRunList();
+    isLoading.value = false;
+  })  
 }
 
 watch(() => userCalibrationRunData.value, (updatedRunData, initialRunData) => {
   if (updatedRunData != undefined && Object.keys(updatedRunData).length > 0) {
     nextTick(() => {
-      isLoadingCalibrationSummary.value = false;
+      isLoading.value = false;
       loadCalibrationDataComplete.value = true;
     });
   }
@@ -156,6 +180,17 @@ const onEvalValdiationRowSelect = async (event: DataTableRowClickEvent) => {
 
 const onEvalValidationRowUnSelect = async (event: DataTableRowClickEvent) => {
   evaluateValidationRunId.value = 0;
+}
+
+const openSelectedCalibrationRun = () => {
+  isLoading.value = true;
+  resetUserSelectedEvalValidationRun();
+   nextTick( async () => {
+    await loadSelectedCalibrationRun(contextMenuJob.value as number);  
+    await fetchUserSelectedCalibrationValidationRunList();
+    isLoading.value = false;
+  })
+  
 }
 
 // const navigateToAlternateIteration = (event: any) => {
@@ -178,6 +213,42 @@ const rowStyle = (data: any) => {
     return { backgroundColor: 'white' }
   }
 }
+
+const confirmDelte = useConfirm();
+const deleteSelectedCalibrationRun = () => {
+  const selectedRunId = contextMenuJob.value as number;
+  let confirmMessage = "Are you sure you want to delete this run?"
+  confirmDelte.require({
+    message: confirmMessage,
+    header: 'Confirm Delete',
+    icon: 'pi pi-exclamation-triangle',
+    rejectProps: {
+      label: 'Cancel',
+      severity: 'secondary',
+      outlined: true
+    },
+    acceptProps: {
+      label: 'DELETE RUN',
+    },
+    accept: () => acceptDelete(selectedRunId),
+    reject: () => {
+      //do nothing
+    }
+  })
+}
+const acceptDelete = (selectedRunId: number) => {
+  deleteCalibrationRun(selectedRunId).then( response => {
+    if ( response.status == 200 ) {
+      fetchUserValidatedCalibrationJobsListData();
+    } else {
+      useApiErrorResponsePreprocess( response ).forEach( message => {
+        toast.add({ severity: useApiResponseToastSeverityCode( response?.status ), summary: 'Delete Calibration Job Failed.', detail: message, life: 10000 });
+      });
+    }
+  });
+  selectedCalibrationRun.value = undefined;
+}
+
 </script>
 
 <style lang="scss" scoped>
