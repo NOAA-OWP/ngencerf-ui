4e025b6c92150dff9dce3596c488176971d4acce
Carolyn.Maynard <carolyn.maynard@rtx.com>
2024-11-22 12:00:11 -0500

    Updates to the Calibration, Calibration Runs tab.

--- components/Help/PreviousRunsHelp.vue
+++ components/Help/PreviousRunsHelp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="_help-page">
-    <div class="_help-title">ngenCERF Calibration Landing Page</div>
-    <div class="_help-subtitle">Displays table of previous calibration run with some basic details.</div>
+    <div class="_help-title">ngenCERF Calibration Runs Tab</div>
+    <div class="_help-subtitle">Display Users Previous Calibration jobs.</div>
     <p class="text-center" style="color:#cc5500;font-size:0.8em;">
       WARNING: Clicking the browser refresh button takes you to the Calibration Runs tab.
     </p>
@@ -9,12 +9,24 @@
     <table class="_help-table">
       <tbody>
         <tr>
-          <td class="td1"></td>
-          <td class="td2"><strong>New Button starts a new calibration setup.</strong></td>
+          <td class="td1">New Button</td>
+          <td class="td2">Starts a new calibration setup.</td>
         </tr>
         <tr>
-          <td class="td1"></td>
-          <td class="td2"><strong>Statuses:</strong></td>
+          <td class="td1">&nbsp;</td>
+          <td class="td2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="td1">Double Click</td>
+          <td class="td2">Loads the selected calibration job. User will be taken to the Run/Status tab. (Same as right click/open)</td>
+        </tr>
+        <tr>
+          <td class="td1">&nbsp;</td>
+          <td class="td2">&nbsp;</td>
+        </tr>
+        <tr>
+          <td class="td1">Statuses</td>
+          <td class="td2"><strong>Description</strong></td>
         </tr>
         <tr>
           <td class="td1">Saved:</td>
@@ -46,22 +58,20 @@
     <table class="_help-table">
       <tbody>
         <tr>
-          <td class="td1">&nbsp;</td>
-          <td class="td2"><strong>User can right click on an entry in the table to:</strong></td>
+          <td class="td1">Right Click</td>
+          <td class="td2"><strong>Provides a context menu with the following options</strong></td>
         </tr>
         <tr>
           <td class="td1">Open:<br /></td>
-          <td class="td2">If job is Ready, Running, Complete, Failed or Cancelled user will be taken to the Results tab,
-            otherwise user will be taken to a setup tab..</td>
+          <td class="td2">Loads the selected calibration job. User will be taken to the Run/Status tab.</td>
         </tr>
         <tr>
           <td class="td1">Clone:</td>
-          <td class="td2">Use the data from the selected calibration job as a baseline for to setup a new calibration.
-            User will be taken to the Headwater Basin Gage tab..</td>
+          <td class="td2">Use the data from the selected calibration job as a baseline for a new calibration that will be added to the table.</td>
         </tr>
         <tr>
           <td class="td1">Delete:</td>
-          <td class="td2">Delete the selected job. User will remain on this page.</td>
+          <td class="td2">Delete the selected calibration job and all it's associated validation jobs. User will remain on this page.</td>
         </tr>
       </tbody>
     </table>
