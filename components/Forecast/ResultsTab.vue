e7ae454b55948e54dc97e8a0b49f49bc9c07bbee
Miguel.Pena <miguel.pena@rtx.com>
2024-12-31 12:43:24 -0800

    updates to Forecast ResultsTab

--- components/Forecast/ResultsTab.vue
+++ components/Forecast/ResultsTab.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="text-center text-2xl mt-20">Results</div>
-  <div class="text-center text-2xl mt-20">
-    <img alt="Page Under Construction" class="m-auto construction-images" src="@/assets/styles/img/under_construction.png" />
-    </div>
+  <div>
+        <div id="GraphArea" class="text-center text-2xl mt-20" v-if="selectedPlotFileUrl">
+          <img :src="selectedPlotFileUrl" alt="Selected Plot" />
+        </div>
+        <div id="GraphArea" class="text-center text-2xl mt-20" v-else>
+          <!--Data Display-->
+        </div>
+      </div>
 </template>
 
 <script setup lang="ts">
-import { hilightTab } from '@/composables/TabHilight';
-onMounted(() => {
-  hilightTab(ForecastTabs.tab_results);
-  
-})
 
+import { hilightTab } from '@/composables/TabHilight';
+import { useToast } from 'primevue/usetoast';
+
+const toast = useToast();
+
+onMounted(() => {
+  toast.removeAllGroups();
+  let ele = document.getElementById("MainLeftDataArea") as HTMLElement;
+  if (ele) { ele.scrollTo(0, 0); }
+
+  hilightTab(ForecastTabs.tab_results);
+});
 
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/styles/styles.scss";
-
-
 </style>
