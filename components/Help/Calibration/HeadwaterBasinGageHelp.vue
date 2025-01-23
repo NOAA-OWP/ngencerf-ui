61c05d670546ebc34cca0cc3b671577b9c02fea0
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-01-23 11:06:50 -0800

    Calibration, Headwater Basin Gage helpfile updates. Fix GeoPackage label.

--- components/Help/Calibration/HeadwaterBasinGageHelp.vue
+++ components/Help/Calibration/HeadwaterBasinGageHelp.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="_help-page">
     <div class="_help-title">Calibration Headwater Basin Gage Tab</div>
-    <div class="_help-subtitle">Select the calibration gage for a headwater basin.</div>
+    <div class="_help-subtitle">Select the calibration gage for a headwater basin, along with the 
+      source for the Forcing Data, Observational Data and GeoPackage. User may upload their own files.
+    </div>
     <p class="text-center" style="color:#cc5500;font-size:0.8em;">
       WARNING: Clicking the browser refresh button takes you to the Calibration Runs tab.
     </p>
@@ -14,51 +16,59 @@
     <table class="_help-table">
       <tbody>
         <tr>
-          <td class="td1">Domain:</td>
+          <td class="td1">Domain</td>
           <td class="td2">Pull down to select the domain of interest. Choices are CONUS, Alaska, Hawaii and Puerto
-            Rico/US
-            Virgin Islands.</td>
+            Rico/US Virgin Islands. This is optional.</td>
         </tr>
         <tr>
-          <td class="td1">Calibration Gage:</td>
-          <td class="td2">Pull down to select a calibration gage from the chosen domain. Typing in this field shows predictive
-            choices.
-            A choice must be made from the pull down list. Required to upload Forcing, Observational or GeoPackage
-            files.
+          <td class="td1">Gage</td>
+          <td class="td2">Pull down to select a calibration gage. If a domain has been selected, only gages
+            from that domain will be avaialbe. Typing in this field shows predictive choices.
+            User must select the choice from the list. Required if Upload is selected for Forcing Data, 
+            Observational Data or the GeoPackage.
           </td>
         </tr>
         <tr>
-          <td class="td1">Forcing:</td>
-          <td class="td2">Pull down to select Forcing data source for the selected gage. Either AORC or Upload to upload from local computer. <br /> <br />
-            A gage must have already been selected to upload a file. Upload allows the user to specify their own csv 
-            file containing hourly time-series details of forcing data. The files (one per catchment in the headwater 
-            basin for the selected gage)
+          <td class="td1">Forcing</td>
+          <td class="td2">Pull down to select Forcing data source for the selected gage. 
+            Either AORC or Upload to upload from local computer. <br />
+            <br />
+            Upload allows the user to specify their own csv file containing hourly time-series details of forcing data.
+            <strong><em>A gage must have already been selected to upload a file.</em></strong><br />
+            <br />
+            There must be one forcing file per catchment in the headwater basin for the selected gage and they
             must be in the following format, with the 1st line being the header identifying the following fields:<br />
             Time, LWDOWN, PFC, Q2D, RAINRATE, SWDOWN, T2D, U2D, V2D<br />
             <br>
-            Example:<br />
+            Example:<span style="font-size:0.9em;"> <em>(Clicking on image makes it larger in a new tab.)</em></span><br />
             <a :href=image1 target="_blank"><img :src=image1 /></a><br />
             <img :src=image2 width="90%" class="mb-2" />
           </td>
         </tr>
         <tr>
-          <td class="td1">Observational:</td>
-          <td class="td2">Pull down to select Observational Data source for the selected gage. Either Data Services for Enterprise
-            Hydrofabric Services or Upload to upload from local computer. <br /> <br />
+          <td class="td1">Observational</td>
+          <td class="td2">Pull down to select Observational Data source for the selected gage. 
+            Either Data Services for Enterprise Data Services or Upload to upload from local computer.<br />
+            <br />
             Upload allows the user to specify their own csv file hourly time-series details of observed gage streamflow data.
-            A gage must have already been selected
-            to upload a file. This file must be in the following format, with the the 1st line being the header
-            identifying the following fields:<br /><br />
+            <strong><em>A gage must have already been selected to upload a file.</em></strong><br />
+            <br />
+            This file must be in the following format, with the the 1st line being the header
+            identifying the following fields:<br />
+            <br />
             <span class="italic">dateTime, q_cms</span><br />
             Example:<br />
             <img :src=image3 /><br />
           </td>
         </tr>
         <tr>
-          <td class="td1">GeoPackage:</td>
+          <td class="td1">GeoPackage</td>
           <td class="td2">Pull down to select the source for the selected gage GeoPackage file. Either Data Services for Enterprise
-            Hydrofabric Services or Upload to upload from local computer.
-            Upload allows the user to specify their own .gpkg file. A gage must have already been selected to upload a file.</td>
+            Data Services or Upload to upload from local computer.<br />
+            <br />
+            Upload allows the user to specify their own .gpkg file. 
+            <strong><em>A gage must have already been selected to upload a file.</em></strong>
+          </td>
         </tr>
         <tr>
           <td class="td1">&nbsp;</td>
