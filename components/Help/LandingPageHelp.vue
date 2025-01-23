a60ae00af7e1c4b98808057e66fe719be3df9acd
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-01-23 10:05:18 -0800

    Landing Page helfile updates

--- components/Help/LandingPageHelp.vue
+++ components/Help/LandingPageHelp.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="_help-page">
-    <div class="_help-title">ngenCERF Landing Page</div>
-    <p style="text-align:center;">
-      This page will display the number of processes that are currently running, if any, along with any calibration runs in process of being setup, but not yet run.
+    <div class="_help-title">Landing Page</div>
+    <p style="line-height:20px;">
+      The app is setup in 4 Workflows: Calibration, Evaluation, Forecast and Verification (aka Reforcast). If a workflow is greyed out,
+      it means it is not yet available.
     </p>
+    <div style="margin-top: 20px; margin-left:40px;">
+      <ul style="list-style-type: disc;">
+        <li> Click on the Calibration button to setup a new Calibration or view previous job or run a calibration.</li>
+        <li> Click on the Evaluation button to evaluate previously run calibration jobs.</li>
+        <li> Click on the Forecast button to run a forecast based off of the best paraemeters from previously run calibration job, or to view a previously run forecast job.</li>
+      </ul>
+    </div>
     <br />
-    <p style="text-align:center;">
-      To setup, run or view a Calibration, click on the <strong>Calibration</strong> button in the header.<br  />
-      To evaluate a Calibration or validation job, or run a validation on an alternate set of parameters click on the <strong>Evaluation</strong> button in the header.
-    </p>
-    <p style="text-align:center;">
-      To view the results of a calibration, validation, or run a new validation, click on the <strong>Evaluate</strong> button in the header.
+    <p style="text-align:center;line-height:20px;">
+      Displays some calibration status such as the number of running jobs, if any, as well as any calibration jobs in setup but not yet run.
     </p>
   </div>
 </template>
