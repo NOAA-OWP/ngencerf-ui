3466d967534d342ff130b50d7ac485bf88a10bd7
Allan.Yu <allan.yu@rtx.com>
2025-01-28 16:00:02 -0800

    fix malformed html in HeadwaterBasinGage and HeadwaterBasinGageHelp

--- components/Help/Calibration/HeadwaterBasinGageHelp.vue
+++ components/Help/Calibration/HeadwaterBasinGageHelp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="_help-page">
-    <div class="_help-title">Calibration Headwater Basin Gage Tab</div>
+    <div class="_help-title" id="help-title">Calibration Headwater Basin Gage Tab</div>
     <div class="_help-subtitle">Select the calibration gage for a headwater basin, along with the 
       source for the Forcing Data, Observational Data and GeoPackage. User may upload their own files.
     </div>
@@ -13,19 +13,15 @@
         starting at the Headwater Basin Gage tab. The user can also simply click a tab or a link in the Progress area to go to that specific setup tab.</em>
     </p>
 
-    <table class="_help-table">
-      <thead>
-        <th>Item</th>
-        <th>Description</th>
-      </thead>      
+    <table class="_help-table" aria-describedby="help-title">   
       <tbody>
         <tr>
-          <td class="td1">Domain</td>
+          <th scope="row" class="td1">Domain</th>
           <td class="td2">Pull down to select the domain of interest. Choices are CONUS, Alaska, Hawaii and Puerto
             Rico/US Virgin Islands. This is optional.</td>
         </tr>
         <tr>
-          <td class="td1">Gage</td>
+          <th scope="row" class="td1">Gage</th>
           <td class="td2">Pull down to select a calibration gage. If a domain has been selected, only gages
             from that domain will be avaialbe. Typing in this field shows predictive choices.
             User must select the choice from the list. Required if Upload is selected for Forcing Data, 
@@ -33,7 +29,7 @@
           </td>
         </tr>
         <tr>
-          <td class="td1">Forcing</td>
+          <th scope="row" class="td1">Forcing</th>
           <td class="td2">Pull down to select Forcing data source for the selected gage. 
             Either AORC or Upload to upload from local computer. <br />
             <br />
@@ -50,7 +46,7 @@
           </td>
         </tr>
         <tr>
-          <td class="td1">Observational</td>
+          <th scope="row" class="td1">Observational</th>
           <td class="td2">Pull down to select Observational Data source for the selected gage. 
             Either Data Services for Enterprise Data Services or Upload to upload from local computer.<br />
             <br />
@@ -66,7 +62,7 @@
           </td>
         </tr>
         <tr>
-          <td class="td1">GeoPackage</td>
+          <th scope="row" class="td1">GeoPackage</th>
           <td class="td2">Pull down to select the source for the selected gage GeoPackage file. Either Data Services for Enterprise
             Data Services or Upload to upload from local computer.<br />
             <br />
@@ -79,16 +75,16 @@
           <td class="td2">&nbsp;</td>
         </tr>
         <tr>
-          <td class="td1">Save Button:</td>
+          <th scope="row" class="td1">Save Button:</th>
           <td class="td2">Save data from the tab. Partial saves are allowed.</td>
         </tr>
         <tr>
-          <td class="td1">Revert Button</td>
+          <th scope="row" class="td1">Revert Button</th>
           <td class="td2">If the saved gage ID is changed, the Revert button will appear. When
             clicked it will undo the change, reverting to the saved gage ID.</td>
         </tr>
         <tr>
-          <td class="td1">Next Button:</td>
+          <th scope="row" class="td1">Next Button:</th>
           <td class="td2">Move to the Formulation Tab. If there is any unsaved data, the user will first be prompted to save.</td>
         </tr>
       </tbody>
