fdf49dc511dded4f17394b325a3f747b636f7b9c
Miguel.Pena <miguel.pena@rtx.com>
2025-01-30 14:51:26 -0800

    remove persist from pinia stores

--- stores/evaluation/EvaluationAltIterationStore.ts
+++ stores/evaluation/EvaluationAltIterationStore.ts
@@ -406,9 +406,6 @@ export const useEvaluationAltIterationStore = defineStore(
       userSelectedCalibrationIterationId,
       createNewValidationJob,
     };
-  },
-  {
-    persist: true,
   }
 );
 
