e79833f4224dfb02e6c151c6e334d0ebf285daa6
norman.strassner <nstrassner@asrcfederal.com>
2025-02-05 11:22:15 -0700

    Added help files that were not added

---
+++ components/Help/Forecast/CalibrationRunsHelp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="_help-page">
+    <div class="_help-title">Forecast - Caliibration Runs Tab</div>
+    <p id="help-subtitle" class="_help-subtitle">
+      &nbsp;
+    </p>
+    <p class="text-center" style="color:#cc5500;font-size:0.8em;">
+      WARNING: 
+    </p>
+    <hr class="mt-2 mb-4" />
+    <table class="_help-table" aria-describedby="help-subtitle">
+      <thead>
+        <tr>
+          <th class="td1">Item</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="td1">Title</td>
+          <td class="td2"><strong>Definition:</strong></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>components/Help/Evaluation/CalibrationRunsHelp.vue
+
+<style lang="scss" scoped>
+@use "@/assets/styles/global.scss";
+@use "@/assets/styles/styles.scss";
+
+.helpfile-ul {
+  list-style-type:disc !important;
+}
+
+._help-table .td1 {
+  width: 300px;
+}
+</style>
