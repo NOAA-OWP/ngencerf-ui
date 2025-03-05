7320f80382ec519feeb600f885b6ea197c086807
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-03-04 16:39:00 -0800

    Add line break before Results Pathname in Calibration Details

--- components/Common/MessagesGroup.vue
+++ components/Common/MessagesGroup.vue
@@ -137,10 +137,10 @@
               class="font-medium">Plot Generation Frequency:</span>
             {{ calData?.save_plot_iteration_frequency }}</div>
         </div>
-	<div class="col-span-2">
+	      <div class="col-span-2">
           <div v-if="resultsPathname"
             :aria-label="'Results Pathname ' + resultsPathname"
-            :title="'Results Pathname ' + resultsPathname"><span class="font-medium">
+            :title="'Results Pathname ' + resultsPathname"><br /><span class="font-medium">
               Results Pathname:</span>
             {{ resultsPathname }}</div>
         </div>
