41e2fc93dba79dc1ffc9b926f967b3f960ffa0d9
norman.strassner <nstrassner@asrcfederal.com>
2025-03-12 13:17:42 -0600

    Link removed

--- pages/Verification.vue
+++ pages/Verification.vue
@@ -6,13 +6,9 @@
           <AppHeader />
         </div>
       </div>
-      <a class="absolute inset-x-[240px] inset-y-[97px] text-white" target="_blank"
-        href="https://confluence.nextgenwaterprediction.com/x/poNtAg">Link to the Confluence Page</a>
       <div class="grid row-span-10 gap-2">
         <iframe class="inline" title="pdf" :src="pdfUrl" width="100%" height="100%"></iframe>
-
       </div>
-
       <div class="row-span-1">
         <AppFooter />
       </div>
@@ -23,7 +19,7 @@
 <script setup lang="ts">
 import AppFooter from "@/components/Common/AppFooter.vue";
 import AppHeader from "@/components/Common/AppHeader.vue";
-import pdfUrl from '@/assets/styles/pdfs/ForecastVerification.pdf';
+import pdfUrl from '@/assets/styles/pdfs/VerificationSteps.pdf';
 
 </script>
 
@@ -31,53 +27,4 @@ import pdfUrl from '@/assets/styles/pdfs/ForecastVerification.pdf';
 @use "@/assets/styles/global.scss";
 @use "@/assets/styles/styles.scss";
 
-a {
-  color: #0052CC;
-  text-decoration: underline;
-  cursor: pointer;
-}
-
-.bluetext {
-  color: #0052CC;
-}
-
-#Verification {
-  padding: 40px;
-}
-
-.section-title {
-  font-size: 1.3em;
-}
-
-.sub-title {
-  font-size: 1.1em;
-  margin: 8px 0;
-}
-
-.instruction {
-  font-weight: 600;
-}
-
-.numbered-list {
-  margin-left: 20px;
-  margin-bottom: 10px;
-}
-
-.boxed-command {
-  border: 1px solid #555;
-  padding: 10px;
-  margin-bottom: 10px;
-}
-
-.container {
-  height: 800px;
-  width: 100%;
-  overflow-x: hidden;
-  overflow-y: auto;
-}
-
-
-.section {
-  margin-bottom: 30px;
-}
 </style>
