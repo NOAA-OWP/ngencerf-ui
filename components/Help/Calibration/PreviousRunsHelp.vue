ab54e0633e5f04911e57b6abf614a372031eb608
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-06-02 17:58:47 -0700

    Updated Calibration, Calibration Runs help

--- components/Help/Calibration/PreviousRunsHelp.vue
+++ components/Help/Calibration/PreviousRunsHelp.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="_help-page">
     <div class="_help-title">Calibration - Calibration Runs Tab</div>
-    <div class="_help-subtitle">Displays a list of users calibration jobs</div>
+    <div class="_help-subtitle" style="text-align: center;">Displays list of calibration jobs</div>
     <p class="text-center" style="color:#cc5500;font-size:0.8em;">
       WARNING: Clicking the browser refresh button takes you to the Calibration Runs tab.
+    </p>
+
+    <p class="text-center">
+      Sort table by clicking on the sort icon in a column.
     </p>
 
     <table class="_help-table" aria-label="Calibration Runs Tab Help Table">
       <thead>
         <tr>
-          <th>Item</th>
+          <th>Action</th>
           <th>Description</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td class="td1">New Button</td>
-          <td class="td2">Starts a new calibration setup.</td>
+          <td class="td2">Click to create a calibration setup from scratch.</td>
         </tr>
         <tr>
           <td class="td1">&nbsp;</td>
           <td class="td2">&nbsp;</td>
         </tr>
         <tr>
-          <td class="td1">Headwater Basin Gage Filter</td>
-          <td class="td2" style="vertical-align:top;">Show only those jobs associated with the selected Gage ID</td>
+          <td class="td1">Filters</td>
+          <td class="td2" style="vertical-align:top;">Filter by Headwater Basin Gage, 
+            multi-select status, multi-select modules, and/or archived jobs. 
+            Must click <b>Apply Filters</b> button to filter the table</td>
+        </tr>
+        <tr>
+          <td class="td1">Clear Filters</td>
+          <td class="td2 align-top">Clears/Resets the filters.</td>
+        </tr>
+        <tr>
+          <td class="td1">Refresh List</td>
+          <td class="td2 align-top">Refreshes the list of filtered jobs.</td>
         </tr>
         <tr>
           <td class="td1">&nbsp;</td>
@@ -32,60 +46,16 @@
         </tr>
         <tr>
           <td class="td1">Double Click</td>
-          <td class="td2">Loads the selected calibration job. User will be taken to the Run/Status tab. (Same as right
-            click/Open)</td>
+          <td class="td2">Opens calibration job and takes user to the Status/Run tab.
+            (Same as right click/Open)</td>
         </tr>
         <tr>
           <td class="td1">&nbsp;</td>
           <td class="td2">&nbsp;</td>
         </tr>
         <tr>
-          <td class="td1">Statuses</td>
-          <td class="td2"><strong>Description</strong></td>
-        </tr>
-        <tr>
-          <td class="td1" style="font-weight:normal;">Saved</td>
-          <td class="td2">Calibration setup has begun, but not yet completed.</td>
-        </tr>
-        <tr>
-          <td class="td1" style="font-weight:normal;">Ready</td>
-          <td class="td2">Calibration setup complete, but not run. The word Calibration may be present.</td>
-        </tr>
-        <tr>
-          <td class="td1" style="font-weight:normal;">Running</td>
-          <td class="td2">When Running is in the status, indicates the Calibration/Validation phase currently running.
-          </td>
-        </tr>
-        <tr>
-          <td class="td1" style="font-weight:normal;">Done</td>
-          <td class="td2">Calibration job completed without any errors.</td>
-        </tr>
-        <tr>
-          <td class="td1" style="font-weight:normal;">Failed</td>
-          <td class="td2">Calibration job failed.</td>
-        </tr>
-        <tr>
-          <td class="td1" style="font-weight:normal;">Cancelled</td>
-          <td class="td2">Calibration job cancelled by user.</td>
-        </tr>
-        <tr>
-          <td class="td1" style="font-weight:normal;">Server Error</td>
-          <td class="td2">The server had an error before the job finished.</td>
-        </tr>
-      </tbody>
-    </table>
-    <hr />
-    <table class="_help-table" aria-label="Calibration Runs Tab Menu Item Help Table">
-      <thead>
-        <tr>
-          <th>Menu Item</th>
-          <th>Description</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
           <td class="td1">Right Click</td>
-          <td class="td2"><strong>Provides a context menu with the following options</strong></td>
+          <td class="td2 align-top"><strong>Shows a context menu with the following options</strong></td>
         </tr>
         <tr>
           <td class="td1" style="font-weight:normal;">Open<br /></td>
@@ -97,9 +67,61 @@
             will be added at the top of the table. User will remain on this page.</td>
         </tr>
         <tr>
+          <td class="td1" style="font-weight:normal;">Download</td>
+          <td class="td2">Download all data associated with a Done calibration job.</td>
+        </tr>
+        <tr>
           <td class="td1" style="font-weight:normal;">Delete</td>
-          <td class="td2">Delete the selected calibration job and all it's associated validation jobs. User will remain
-            on this page.</td>
+          <td class="td2">Permanently deletes the selected calibration job(s) from the system.</td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Archive/Unarchive</td>
+          <td class="td2">Archive or Unarchive the selected calibration job(s). 
+            Archived jobs will no longer appear in any Calibration Runs table unless the Archived filter is active.</td>
+        </tr>
+      </tbody>
+    </table>
+    <hr class="mt-3" style="height: 2px; background-color: black" />
+    <div class="mt-3 text-center font-bold text-xl">Status Definitions</div>
+    <table class="_help-table" aria-label="Status Definitions">
+      <thead>
+        <tr class="text-left">
+          <th style="text-align:center;">Status</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="td1"></td>
+          <td class="td2"><strong></strong></td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Saved</td>
+          <td class="td2 align-top">Calibration setup has begun, but not yet complete.</td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Ready</td>
+          <td class="td2 align-top">Calibration setup complete, but not run. The word Calibration may be present.</td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Running</td>
+          <td class="td2 align-top">Indicates the Calibration/Validation is running. User can view progress and iteration plots.</td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Done</td>
+          <td class="td2 align-top">Calibration job completed without any errors.</td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Failed</td>
+          <td class="td2 align-top">Calibration job failed.</td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Cancelled</td>
+          <td class="td2 align-top">Calibration job cancelled by user.</td>
+        </tr>
+        <tr>
+          <td class="td1" style="font-weight:normal;">Server Error</td>
+          <td class="td2 align-top">The server had an error before the job finished.</td>
         </tr>
       </tbody>
     </table>
@@ -118,6 +140,13 @@
       </thead>
       <tbody>
         <tr class="leading-8">
+          <td style="background-color: yellow" class="pr-1">&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>Yellow</td>
+          <td>&nbsp;</td>
+          <td>Saved</td>
+        </tr>
+        <tr class="leading-8">
           <td style="background-color: green" class="pr-1">&nbsp;</td>
           <td>&nbsp;</td>
           <td>Green</td>
@@ -125,11 +154,11 @@
           <td>Ready</td>
         </tr>
         <tr class="leading-8">
-          <td style="background-color: yellow" class="pr-1">&nbsp;</td>
+          <td style="background-color: green" class="pr-1 flash-green">&nbsp;</td>
           <td>&nbsp;</td>
-          <td>Yellow</td>
+          <td>Flashing Green</td>
           <td>&nbsp;</td>
-          <td>Saved</td>
+          <td>Running</td>
         </tr>
         <tr class="leading-8">
           <td style="background-color: blue" class="pr-1">&nbsp;</td>
@@ -153,13 +182,6 @@
           <td>Failed</td>
         </tr>
         <tr class="leading-8">
-          <td style="background-color: green" class="pr-1">&nbsp;</td>
-          <td>&nbsp;</td>
-          <td>Flashing Green</td>
-          <td>&nbsp;</td>
-          <td>Running</td>
-        </tr>
-          <tr class="leading-8">
           <td style="background-color: black" class="pr-1">&nbsp;</td>
           <td>&nbsp;</td>
           <td>Black</td>
@@ -169,37 +191,7 @@
       </tbody>
     </table>
 
-    <hr class="mt-3" style="height: 2px; background-color: black" />
-
-    <div class="mt-3 text-center font-bold text-xl">Job Filter</div>
-    <p class="text-center">
-      Calibration Job Filters allows the user to display only those jobs that meet certain criteria.
-    </p>
-    <div class="ml-8 mt-3">
-      <div class="font-bold">Filters are applied in this manner:</div>
-      <ul class="mt-4">
-        <li>The base collection of jobs is determined by the value of the Headwater Basic Gage dropdown, which allows
-          the job list
-          to be filtered for a specific gage, or "All" to display all jobs returned by the server.
-        </li>
-        <li>After the job list is optionally filtered by Headwater Basic Gage, one or more Status selections can be made
-          to further filter the job list.
-          If there are no Status items selected, the filter will be bypassed.
-        </li>
-        <li>Lastly, if no Modules are slected, no further jobs will be filtered from the list, but the user may select
-          one or more Modules to further filter the job list.
-        </li>
-      </ul>
     </div>
-    <div class="ml-8 mt-3">
-      <h2>Archived Jobs</h2>
-      <p>If the <strong>Include Archived</strong> checkbox is checked then the job list will include any and all jobs
-        that have been archived.</p>
-      <p>To archive a job, right click on an un-archived job and select "Archive". </p>
-      <p>If a job has been previousl archived, you can right click and choose "Un-archive".</p>
-    </div>
-
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -216,5 +208,25 @@
 
 p {
   margin-left: 50px;
+}
+</style>
+
+<style scoped>
+/* Define the flashing animation */
+@keyframes flashGreen {
+    0% {
+        background-color: green;
+    }
+    50% {
+        background-color: white;
+    }
+    100% {
+        background-color: green;
+    }
+}
+
+/* Apply the animation to the <td> element */
+.flash-green {
+    animation: flashGreen 1s linear infinite;
 }
 </style>
