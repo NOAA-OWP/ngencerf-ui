b397059b25bef7c478c365036917456932603519
Carolyn.Maynard <carolyn.maynard@rtx.com>
2025-06-04 08:48:45 -0700

    td cleanup

--- components/Help/Calibration/PreviousRunsHelp.vue
+++ components/Help/Calibration/PreviousRunsHelp.vue
@@ -5,78 +5,78 @@
     <p class="text-center" style="color:#cc5500;font-size:0.8em;">
       WARNING: Clicking the browser refresh button takes you to the Calibration Runs tab.
     </p>
-
+    <br />
     <p class="text-center">
       Sort table by clicking on the sort icon in a column.
     </p>
-
+    <br />
     <table class="_help-table" aria-label="Calibration Runs Tab Help Table">
       <thead>
         <tr>
-          <th>Action</th>
+          <th class="text-left">Action</th>
           <th>Description</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td class="td1">New Button</td>
-          <td class="td2">Click to create a calibration setup from scratch.</td>
+          <td class="td1 align-top">New Button</td>
+          <td class="td2 align-top">Click to create a calibration setup from scratch.</td>
         </tr>
         <tr>
-          <td class="td1">&nbsp;</td>
-          <td class="td2">&nbsp;</td>
+          <td class="td1 align-top">&nbsp;</td>
+          <td class="td2 align-top">&nbsp;</td>
         </tr>
         <tr>
-          <td class="td1">Filters</td>
-          <td class="td2" style="vertical-align:top;">Filter by Headwater Basin Gage, 
+          <td class="td1 align-top">Filters</td>
+          <td class="td2 align-top" style="vertical-align:top;">Filter by Headwater Basin Gage, 
             multi-select status, multi-select modules, and/or archived jobs. 
             Must click <b>Apply Filters</b> button to filter the table</td>
         </tr>
         <tr>
-          <td class="td1">Clear Filters</td>
+          <td class="td1 align-top">Clear Filters</td>
           <td class="td2 align-top">Clears/Resets the filters.</td>
         </tr>
         <tr>
-          <td class="td1">Refresh List</td>
+          <td class="td1 align-top">Refresh List</td>
           <td class="td2 align-top">Refreshes the list of filtered jobs.</td>
         </tr>
         <tr>
-          <td class="td1">&nbsp;</td>
-          <td class="td2">&nbsp;</td>
+          <td class="td1 align-top">&nbsp;</td>
+          <td class="td2 align-top">&nbsp;</td>
         </tr>
         <tr>
-          <td class="td1">Double Click</td>
-          <td class="td2">Opens calibration job and takes user to the Status/Run tab.
+          <td class="td1 align-top">Double Click</td>
+          <td class="td2 align-top">Opens calibration job and takes user to the Status/Run tab.
             (Same as right click/Open)</td>
         </tr>
         <tr>
-          <td class="td1">&nbsp;</td>
-          <td class="td2">&nbsp;</td>
+          <td class="td1 align-top">&nbsp;</td>
+          <td class="td2 align-top">&nbsp;</td>
         </tr>
         <tr>
-          <td class="td1">Right Click</td>
+          <td class="td1 align-top">Right Click</td>
           <td class="td2 align-top"><strong>Shows a context menu with the following options</strong></td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Open<br /></td>
-          <td class="td2">Loads the selected calibration job. User will be taken to the Run/Status tab.</td>
+          <td class="td1 align-top" style="font-weight:normal;">Open<br /></td>
+          <td class="td2 align-top">Loads the selected calibration job. User will be taken to the Run/Status tab.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Clone</td>
-          <td class="td2">Use the data from the selected calibration job as a baseline for a new calibration that
+          <td class="td1 align-top" style="font-weight:normal;">Clone</td>
+          <td class="td2 align-top">Use the data from the selected calibration job as a baseline for a new calibration that
             will be added at the top of the table. User will remain on this page.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Download</td>
-          <td class="td2">Download all data associated with a Done calibration job.</td>
+          <td class="td1 align-top" style="font-weight:normal;">Download</td>
+          <td class="td2 align-top">Download all data associated with a Done calibration job.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Delete</td>
-          <td class="td2">Permanently deletes the selected calibration job(s) from the system.</td>
+          <td class="td1 align-top" style="font-weight:normal;">Delete</td>
+          <td class="td2 align-top">Permanently deletes the selected calibration job(s) from the system.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Archive/Unarchive</td>
-          <td class="td2">Archive or Unarchive the selected calibration job(s). 
+          <td class="td1 align-top" style="font-weight:normal;">Archive/<br />Unarchive</td>
+          <td class="td2 align-top">Archive or Unarchive the selected calibration job(s). 
             Archived jobs will no longer appear in any Calibration Runs table unless the Archived filter is active.</td>
         </tr>
       </tbody>
@@ -86,37 +86,37 @@
     <table class="_help-table" aria-label="Status Definitions">
       <thead>
         <tr class="text-left">
-          <th style="text-align:center;">Status</th>
+          <th>Status</th>
           <th>Description</th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td class="td1"></td>
-          <td class="td2"><strong></strong></td>
+          <td class="td1 align-top"></td>
+          <td class="td2 align-top"><strong></strong></td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Saved</td>
+          <td class="td1 align-top" style="font-weight:normal;">Saved</td>
           <td class="td2 align-top">Calibration setup has begun, but not yet complete.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Ready</td>
+          <td class="td1 align-top" style="font-weight:normal;">Ready</td>
           <td class="td2 align-top">Calibration setup complete, but not run. The word Calibration may be present.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Running</td>
+          <td class="td1 align-top" style="font-weight:normal;">Running</td>
           <td class="td2 align-top">Indicates the Calibration/Validation is running. User can view progress and iteration plots.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Done</td>
+          <td class="td1 align-top" style="font-weight:normal;">Done</td>
           <td class="td2 align-top">Calibration job completed without any errors.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Failed</td>
+          <td class="td1 align-top" style="font-weight:normal;">Failed</td>
           <td class="td2 align-top">Calibration job failed.</td>
         </tr>
         <tr>
-          <td class="td1" style="font-weight:normal;">Cancelled</td>
+          <td class="td1 align-top" style="font-weight:normal;">Cancelled</td>
           <td class="td2 align-top">Calibration job cancelled by user.</td>
         </tr>
         <tr>
@@ -201,6 +201,7 @@
 <style scoped>
 ._help-table .td1 {
   width: auto;
+  min-width: 110px;
 }
 
 p {
