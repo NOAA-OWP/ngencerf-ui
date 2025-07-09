975039d4c72c62264a63af6ee2448ee5c7266097
Miguel.Pena <miguel.pena@rtx.com>
2025-07-08 17:49:15 -0700

    formatting fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -47,7 +47,7 @@ export default defineNuxtConfig({
       theme: {
         preset: Aura,
         options: {
-            darkModeSelector: false,
+          darkModeSelector: false,
         }
       }
     }
